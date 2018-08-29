2459a65d3004fbdc9ce91b106b6126941e4bfc95
raquelv85 <raquelv85@gmail.com>
2018-08-29 12:30:58 +0200

    add project

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-console.log('>> Ready :)');
+
+let links = document.querySelectorAll('.link-section');
+let lis = document.querySelectorAll('.item');
+
+function scrollSpy(e) {
+  let item = e.currentTarget;
+
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].classList.remove('item--check');
+  }
+  item.parentElement.classList.add('item--check');
+}
+
+
+for (let i = 0; i < links.length; i++) {
+
+  links[i].addEventListener('click', scrollSpy);
+}
+
+
+
+
+
+
+
+
+
+
+
+     
+
+
+
