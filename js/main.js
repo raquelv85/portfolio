c57dd0ce413935cb5adf27b1d552cccd88786843
Raquel Villaverde <raquelv85@gmail.com>
2018-08-30 21:18:49 +0200

    change js

--- js/main.js
+++ js/main.js
@@ -3,28 +3,55 @@
 
 let links = document.querySelectorAll('.link-section');
 let lis = document.querySelectorAll('.item');
+let sections = document.querySelectorAll('.section');
+let objSections = {};
 
-function scrollSpy(e) {
-  let item = e.currentTarget;
+// function scrollSpy(e) {
+//   let item = e.currentTarget;
 
-  for (let i = 0; i < lis.length; i++) {
-    lis[i].classList.remove('item--check');
+//   for (let i = 0; i < lis.length; i++) {
+//     lis[i].classList.remove('item--check');
+//   }
+//   item.parentElement.classList.add('item--check');
+// }
+
+
+// for (let i = 0; i < links.length; i++) {
+
+//   links[i].addEventListener('click', scrollSpy);
+// }
+
+
+
+for(let i = 0; i < sections.length; i++){
+    console.dir(sections[i])
+    objSections[sections[i].id] = sections[i].offsetTop;
+    //objSections[i].position = sections[i].offsetTop;
+    //console.log(objSections)    
+}
+
+
+window.addEventListener('scroll', function(e){
+ 
+  let  scrollPosition = document.documentElement.scrollTop;
+ 
+
+  for(let i in objSections){
+    if(objSections[i] <= scrollPosition){
+      //console.dir(objSections[i])
+      for(let j=0; j<links.length; j++){
+        console.log("atribute",typeof(links[j].getAttribute('data-section')))
+        console.log("i",i)
+        if(links[j].getAttribute('data-section') === i){
+
+          console.log("seccion",links[j].getAttribute('data-section'))
+        }
+      }
+      
+    }
   }
-  item.parentElement.classList.add('item--check');
-}
 
-
-for (let i = 0; i < links.length; i++) {
-
-  links[i].addEventListener('click', scrollSpy);
-}
-
-
-
-
-
-
-
+});
 
 
 
